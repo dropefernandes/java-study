a40a0d8d9f299fa910bebe8ac3e8d8de68b184ca
Pedro Fernandes <pedrofernandes@ciclic.com.br>
2022-09-29 16:30:25 -0300

    feat: for each loop

---
+++ ForEach/application/Program.java
@@ -0,0 +1,12 @@
+package ForEach.application;
+
+public class Program {
+
+  public static void main(String[] args) {
+
+    String[] names = new String[] { "Pedro", "Rafaela", "Fiorella" };
+
+    for (String name : names)
+      System.out.println(name);
+  }
+}
