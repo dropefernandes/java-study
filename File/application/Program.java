ffc5efacbd7e0f4638578ca8ed2c5aa5a785a720
Pedro Fernandes <pedrofernandes@ciclic.com.br>
2022-10-03 18:04:01 -0300

    chore: block try-with-resources

--- File/application/Program.java
+++ File/application/Program.java
@@ -47,5 +47,16 @@ public class Program {
     // e.printStackTrace();
     // }
     // }
+
+    // String path = "C:\\temp\\in.txt";
+    // try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+    // String line = br.readLine();
+    // while (line != null) {
+    // System.out.println(line);
+    // line = br.readLine();
+    // }
+    // } catch (IOException e) {
+    // System.out.println("Error: " + e.getMessage());
+    // }
   }
 }
