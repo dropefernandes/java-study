f09d5135e1c60ec5e6bf6b592ce589b453e65f0f
Pedro Fernandes <pedrofernandes@ciclic.com.br>
2022-10-03 18:00:43 -0300

    feat: file, fileReader and bufferedReader

---
+++ File/application/Program.java
@@ -0,0 +1,51 @@
+package File.application;
+
+// import java.io.BufferedReader;
+import java.io.File;
+// import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    File file = new File("/Users/pedrofernandes/Developments/drope/java/File/text.txt");
+    Scanner sc = null;
+
+    try {
+      sc = new Scanner(file);
+      while (sc.hasNextLine()) {
+        System.out.println(sc.nextLine());
+      }
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
+    } finally {
+      if (sc != null) {
+        sc.close();
+      }
+    }
+
+    // BufferedReader br = null;
+    // FileReader fr = null;
+    // try {
+    // fr = new FileReader(path);
+    // br = new BufferedReader(fr);
+    // String line = br.readLine();
+    // while (line != null) {
+    // System.out.println(line);
+    // line = br.readLine();
+    // }
+    // } catch (IOException e) {
+    // System.out.println("Error: " + e.getMessage());
+    // } finally {
+
+    // try {
+    // if (br != null)
+    // br.close();
+    // if (fr != null)
+    // fr.close();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // }
+  }
+}
