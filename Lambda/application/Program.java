155349700319bc270689e4b92ed2da6a1832f727
Pedro Fernandes <pedrofernandes@ciclic.com.br>
2022-10-06 17:49:11 -0300

    feat: lambda implementation 3

--- Lambda/application/Program.java
+++ Lambda/application/Program.java
@@ -11,14 +11,12 @@ public class Program {
 
     List<Product> products = new ArrayList<>();
 
-    products.add(new Product("Tv", 900.00));
+    products.add(new Product("TV", 900.00));
     products.add(new Product("Notebook", 1200.00));
     products.add(new Product("Tablet", 450.00));
 
-    Comparator<Product> comparator = (prev, next) -> prev.getName().toUpperCase()
-        .compareTo(next.getName().toUpperCase());
-
-    products.sort(comparator);
+    products.sort((productActual, productNext) -> productActual.getName().toUpperCase()
+        .compareTo(productNext.getName().toUpperCase()));
 
     for (Product product : products) {
       System.out.println(product);
