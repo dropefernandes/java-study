f7796d4722592a47663fcaeb04593172c715093f
Pedro Fernandes <pedrofernandes@ciclic.com.br>
2022-10-06 17:42:23 -0300

    feat: lambda anonymous class

--- Lambda/application/Program.java
+++ Lambda/application/Program.java
@@ -1,6 +1,7 @@
 package Lambda.application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import Lambda.entities.Product;
@@ -10,11 +11,18 @@ public class Program {
 
     List<Product> products = new ArrayList<>();
 
-    products.add(new Product("TV", 900.00));
+    products.add(new Product("Tv", 900.00));
     products.add(new Product("Notebook", 1200.00));
     products.add(new Product("Tablet", 450.00));
 
-    products.sort(new MyComparator());
+    Comparator<Product> comparator = new Comparator<Product>() {
+      @Override
+      public int compare(Product p1, Product p2) {
+        return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+      }
+    };
+
+    products.sort(comparator);
 
     for (Product product : products) {
       System.out.println(product);
